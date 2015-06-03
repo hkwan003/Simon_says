8b6940f17beb3cf968782e0ef898756a2a6be915
Calvin Kwan <hkwan003@ucr.edu>
2015-06-02 21:08:13 -0700

    added all lines to sublime

--- final_pro.c
+++ final_pro.c
@@ -1,20 +1,233 @@
-unsigned char var2, var3;
-var2 = 0;
-var3 = 0;
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+//--------Task scheduler data structure---------------------------------------
+// Struct for Tasks represent a running process in our simple real-time operating system.
+typedef struct _task 
+{
+	/*Tasks should have members that include: state, period,
+		a measurement of elapsed time, and a function pointer.*/
+	signed char state; //Task's current state
+	unsigned long int period; //Task period
+	unsigned long int elapsedTime; //Time elapsed since last task tick
+	int (*TickFct)(int); //Task tick function
+} task;
+//--------End Task scheduler data structure-----------------------------------
+
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+unsigned char var = 0;
+unsigned char var2 = 0;
+unsigned char x;
+unsigned char global_g = 0;
+unsigned char time;
 
 
-enum SM2_States {SM2_off, SM2_on1, SM2_on2, SM2_on3, SM2_on4, SM2_on5, SM2_on6} SM2_States;
+void TimerOn() 
+{
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02; 
+	TCNT1=0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80; 
+}
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+void TimerISR() {
+	TimerFlag = 1;
+}
+ISR(TIMER1_COMPA_vect) 
+{
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) 
+	{
+		TimerISR(); 
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M) 
+{
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+
+enum SM1_States { SM1_off, SM1_on1, SM1_on2, SM1_on3, SM1_on4} SM1_State;
+
+void TickFct_State_machine_1() 
+{
+	switch(SM1_State) 
+	{
+
+		case SM1_off:
+		if(global_g == 0)
+		{
+			if(~PINB & 0x10)
+			{
+				SM1_State = SM1_on1;
+			}
+			else
+			{
+				SM1_State = SM1_off;
+			}
+		}
+		break;
+		case SM1_on1:
+		SM1_State = SM1_on2;
+		break;
+		case SM1_on2:
+		SM1_State = SM1_on3;
+		break;
+		case SM1_on3:
+		SM1_State = SM1_on4;
+		break;
+		case SM1_on4:
+		SM1_State = SM1_off;
+		global_g = 1;
+	}
+
+	switch(SM1_State) 
+	{
+		case SM1_off:
+		PORTA = 0X00;
+		break;
+		case SM1_on1:
+		PORTA = 0x01;
+		break;
+		case SM1_on2:
+		PORTA = 0x02;
+		break;
+		case SM1_on3:
+		PORTA = 0x04;
+		break;
+		case SM1_on4:
+		PORTA = 0x08;
+		break;
+		default:
+		break;
+	}
+
+}
+
+enum Press_States {Init, Press1, Press2, Press3, Press4, Error, Off} Press_States;
+void ButtonPress()
+{
+	switch(Press_States)
+	{
+		case Init:
+		if(global_g == 1)
+		{
+			if(~PINB & 0x01)
+			{
+				while(~PINB & 0x01){}
+				Press_States = Press1;	
+			}
+			if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
+			{
+				Press_States = Error;
+			}
+		}
+		break;
+		
+		case Press1:
+		if(~PINB & 0x02)
+		{
+			while(~PINB & 0x02){}
+			Press_States = Press2;
+		}
+		if(~PINB & 0x01 || ~PINB & 0x04 || ~PINB & 0x08)
+		{
+			Press_States = Error;
+		}
+		break;
+		case Press2:
+		if(~PINB & 0x04)
+		{
+			while(~PINB & 0x04){}
+			Press_States = Press3;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x01 || ~PINB & 0x08)
+		{
+			Press_States = Error;
+		}
+		break;
+		case Press3:
+		if(~PINB & 0x08)
+		{
+			while(~PINB & 0x08){}
+			Press_States = Press4;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x01)
+		{
+			Press_States = Error;
+		}
+		break;
+		case Press4:
+		if(~PINB & 0x10)
+		{
+			Press_States = Off;	
+		}
+		break;
+		case Error:
+		if(~PINB & 0x01)
+		{
+			while(~PINB & 0x01){}
+			Press_States = Press1;
+		}
+		break;
+	}
+	switch(Press_States)
+	{
+		case Error:
+		PORTA = 0x10;
+		break;
+		
+		case Press1:
+		PORTA = 0x01;
+		break;
+		
+		case Press2:
+		PORTA = 0x02;
+		break;
+		
+		case Press3:
+		PORTA = 0x04;
+		break;
+		
+		case Press4:
+		PORTA = 0x08;
+		break;
+		case Off:
+		PORTA = 0x00;
+		global_g = 2;
+		default:
+		break;
+		
+	}
+}
+
+enum SM2_State {SM2_off, SM2_on1, SM2_on2, SM2_on3, SM2_on4, SM2_on5, SM2_on6} SM2_States;
 void TickFct_Machine2()
 {
 	switch(SM2_States)
 	{
 		case SM2_off:
-		if(~PINB & 0x10)
+		if(global_g == 2)
 		{
-			SM2_States = SM2_on1;
+			if(~PINB & 0x01)
+			{
+				SM2_States = SM2_on1;
+			}
+			while(!(~PINB & 0x01))
+			{
+				SM2_States = SM2_off;
+			}	
 		}
-		else
-		{
+		else{
 			SM2_States = SM2_off;
 		}
 		break;
@@ -34,10 +247,9 @@ void TickFct_Machine2()
 		SM2_States = SM2_on6;
 		break;
 		case SM2_on6:
+		global_g = 3;
 		SM2_States = SM2_off;
 		break;
-		default;
-		SM2_States = SM2_off;
 	}
 
 	switch(SM2_States)
@@ -66,210 +278,434 @@ void TickFct_Machine2()
 	}
 }
 
-enum Press_state2{Init2, 2Press1, 2Press2, 2Press3, 2Press4, 2Press5, 2Press6, Error2, Off2} Press_state2;
+enum Press_sec_level{Init2, Press_state1, Press_state2, Press_state3, Press_state4, Press_state5, Press_state6, Error2, Off2} Press_sec_level;
 void ButtonPress2()
 {
-	switch(Press_state2)
+	switch(Press_sec_level)
 	{
 		case Init2:
 		if(~PINB & 0x01)
 		{
 			while(~PINB & 0x01){}
-				Press_state2 = 2Press1;
+			Press_sec_level = Press_state1;
 		}
 		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
 		{
-			Press_States = Error2;
+			Press_sec_level = Error2;
 		}
 		break;
-
-		case 2Press1:
+		case Press_state1:
 		if(~PINB & 0x04)
 		{
 			while(~PINB & 0x04){}
-			Press_state2 = 2Press2;
+			Press_sec_level = Press_state2;
 		}
 		if(~PINB & 0x02 || ~PINB & 0x01 || ~PINB & 0x08)
 		{
-			Press_States = Error;
+			Press_sec_level = Error;
 		}
 		break;
 
-		case 2Press2:
+		case Press_state2:
 		if(~PINB & 0x08)
 		{
 			while(~PINB & 0x08){}
-			Press_state2 = 2Press3;
+			Press_sec_level = Press_state3;
 		}
 		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x01)
 		{
-			Press_state2 = Error2;
+			Press_sec_level = Error2;
 		}
 		break;
 
-		case 2Press3:
+		case Press_state3:
 		if(~PINB & 0x01)
 		{
 			while(~PINB & 0x01){}
-			Press_state2 = 2Press4;
+			Press_sec_level = Press_state4;
 		}
 		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
 		{
-			Press_state2 = Error2;
+			Press_sec_level = Error2;
 		}
 		break;
 
-		case 2Press4:
+		case Press_state4:
 		if(~PINB & 0x02)
 		{
 			while(~PINB & 0x02){}
-			Press_state2 = 2Press5;
+			Press_sec_level = Press_state5;
 		}
 		if(~PINB & 0x01 || ~PINB & 0x04 || ~PINB & 0x08)
 		{
-			Press_state2 = Error2;
+			Press_sec_level = Error2;
 		}
 		break;
 
-		case 2Press5:
+		case Press_state5:
 		if(~PINB & 0x01)
 		{
 			while(~PINB & 0x01){}
-			Press_state2 = 2Press6;
+			Press_sec_level = Press_state6;
 		}
 		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
 		{
-			Press_state2 = Error2;
+			Press_sec_level = Error2;
 		}
 		break;
 
-		case 2Press6:
+		case Press_state6:
 		if(~PINB & 0x10)
 		{
-			Press_state2 = Off2;
+			Press_sec_level = Off2;
 		}
 		break;
 		default:
-		Press_state2 = Init2;
+		Press_sec_level = Init2;
 	}
-	switch(Press_state2)
+	switch(Press_sec_level)
 	{
 		case Error2:
 		PORTA = 0x10;
 		break;
 
-		case 2Press1:
+		case Press_state1:
 		PORTA = 0x01;
-		var2++;
 		break;
 
-		case 2Press2:
-		PORTA 0x04;
-		var2++;
+		case Press_state2:
+		PORTA = 0x04;
 		break;
 
-		case 2Press3:
+		case Press_state3:
 		PORTA = 0x08;
-		var2++;
 		break;
 
-		case 2Press4:
+		case Press_state4:
 		PORTA = 0x01;
-		var2++;
 		break;
 
-		case 2Press5:
+		case Press_state5:
 		PORTA = 0x02;
-		var2++;
 		break;
 
-		case 2Press6:
+		case Press_state6:
 		PORTA = 0x01;
-		var2++;
 		break;
 
 		case Off2:
 		PORTA = 0x00;
-
+		global_g = 4;
 		default:
 		break;
 
 	}
 }
 
-
-enum SM3_States {SM3_off, SM3_on1, SM3_on2, SM3_on3, SM3_on4, SM3_on5, SM3_on6, SM3_on7, SM3_on8, SM3_on9} SM3_States;
+enum SM3_States {SM3_off, SM3_on1, SM3_on2, SM3_on3, SM3_on4, SM3_on5, SM3_on6, SM3_on7, SM3_on8, SM3_on9} SM3_State;
 void TickFct_Machine3()
 {
-	switch(SM3_States)
+	switch(SM3_State)
 	{
 		case SM3_off:
+		if(global_g == 4)
+		{
+			PORTA = 0x04;
+			if(~PINB & 0x01)
+			{
+				while(~PINB & 0x01){}
+				SM3_State = SM3_on1;
+			}
+			else
+			{
+				SM3_State = SM3_off;
+			}	
+		}
+		break;
+		case SM3_on1:
+		SM3_State = SM3_on2;
+		break;
+		case SM3_on2:
+		SM3_State = SM3_on3;
+		break;
+		case SM3_on3:
+		SM3_State = SM3_on4;
+		break;
+		case SM3_on4:
+		SM3_State = SM3_on5;
+		break;
+		case SM3_on5:
+		SM3_State = SM3_on6;
+		break;
+		case SM3_on6:
+		SM3_State = SM3_on7;
+		break;
+		case SM3_on7:
+		SM3_State = SM3_on8;
+		break;
+		case SM3_on8:
+		SM3_State = SM3_on9;
+		break;
+		case SM3_on9:
+		SM3_State = SM3_off;
+	}
+	switch(SM3_State)
+	{
+		case SM3_off:
+		PORTA = 0x00;
+		break;
+		case SM3_on1:
+		PORTA = 0x02;
+		break;
+		case SM3_on2:
+		PORTA = 0x04;
+		break;
+		case SM3_on3:
+		PORTA = 0x01;
+		break;
+		case SM3_on4:
+		PORTA = 0x08;
+		break;
+		case SM3_on5:
+		PORTA = 0x02;
+		break;
+		case SM3_on6:
+		PORTA = 0x04;
+		break;
+		case SM3_on7:
+		PORTA = 0x01;
+		break;
+		case SM3_on8:
+		PORTA = 0x02;
+		break;
+		case SM3_on9:
+		PORTA = 0x08;
+		global_g = 5;
+		break;
+	}
+}
+
+enum Press_third_level{Init3, Press3_state1, Press3_state2, Press3_state3, Press3_state4, Press3_state5, Press3_state6, Press3_state7, Press3_state8, Press3_state9, Error3, Off3} Press_third_level;
+void ButtonPress3()
+{
+	switch(Press_third_level)
+	{
+		case Init3:
+		if(global_g == 5)
+		{
+			PORTA = 0x00;
+			if(~PINB & 0x02)
+			{
+				while(~PINB & 0x02){}
+				Press_third_level = Press3_state1;
+			}
+			if(~PINB & 0x01 || ~PINB & 0x04 || ~PINB & 0x08)
+			{
+				Press_third_level = Error3;
+			}
+		}
+		break;
+		case Error3:
+		if(~PINB & 0x02)
+		{
+			while(~PINB & 0x02){}
+			Press_third_level = Press3_state1;
+		}
+		break;
+		case Press3_state1:
+		if(~PINB & 0x04)
+		{
+			
+			while(~PINB & 0x04){}
+			Press_third_level = Press3_state2;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x01 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state2:
+		if(~PINB & 0x01)
+		{
+			while(~PINB & 0x01){}
+			Press_third_level = Press3_state3;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state3:
+		if(~PINB & 0x08)
+		{
+			while(~PINB & 0x08){}
+			Press_third_level = Press3_state4;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x01)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state4:
+		if(~PINB & 0x02)
+		{
+			while(~PINB & 0x02){}
+			Press_third_level = Press3_state5;
+		}
+		if(~PINB & 0x01 || ~PINB & 0x04 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state5:
+		if(~PINB & 0x04)
+		{
+			
+			while(~PINB & 0x04){}
+			Press_third_level = Press3_state6;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x01 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state6:
+		if(~PINB & 0x01)
+		{
+			while(~PINB & 0x01){}
+			Press_third_level = Press3_state7;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state7:
+		if(~PINB & 0x02)
+		{
+			while(~PINB & 0x02){}
+			Press_third_level = Press3_state8;
+		}
+		if(~PINB & 0x01 || ~PINB & 0x04 || ~PINB & 0x08)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state8:
+		if(~PINB & 0x08)
+		{
+			while(~PINB & 0x08){}
+			Press_third_level = Press3_state9;
+		}
+		if(~PINB & 0x02 || ~PINB & 0x04 || ~PINB & 0x01)
+		{
+			Press_third_level = Error3;
+		}
+		break;
+		case Press3_state9:
 		if(~PINB & 0x10)
 		{
-			SM3_States = SM3_on1;
-		}
-		else
-		{
-			SM3_States = SM3_off;
+			Press_third_level = Off3;
 		}
 		break;
-
-		case SM3_on1:
-		SM3_States = SM3_on2;
+	}
+	switch(Press_third_level)
+	{
+		case Error3:
+		PORTA = 0x10;
 		break;
-
-		case SM3_on2:
-		SM3_States = SM3_on3;
+		case Press3_state1:
+		PORTA = 0x02;
 		break;
-
-		case SM3_on3:
-		SM3_States = SM3_on4;
+		case Press3_state2:
+		PORTA = 0x04;
 		break;
-
-		case SM3_on4:
-		SM3_States = SM3_on5;
+		case Press3_state3:
+		PORTA = 0x01;
 		break;
-
-		case SM3_on5:
-		SM3_States = SM3_on6;
+		case Press3_state4:
+		PORTA = 0x08;
 		break;
-
-		case SM3_on6:
-		SM3_States = SM3_on7;
+		case Press3_state5:
+		PORTA = 0x02;
 		break;
-
-		case SM3_on7:
-		SM3_States = SM3_on8;
+		case Press3_state6:
+		PORTA = 0x04;
 		break;
-
-		case SM3_on8:
-		SM3_States = SM3_on9;
+		case Press3_state7:
+		PORTA = 0x01;
 		break;
-
-		case SM3_on9:
-		SM3_States = SM3_off;
-
-		default:
-		SM3_States = SM3_off;
-
+		case Press3_state8:
+		PORTA = 0x02;
+		break;
+		case Press3_state9:
+		PORTA = 0x08;
+		break;
+		case Off3:
+		PORTA = 0x00;
+		global_g = 6;
+		break;
 	}
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(void)
+{
+	DDRA = 0xff;	PORTA = 0x00;
+	DDRB = 0x00;	PORTB = 0xFF;
+	
+	if(global_g == 0)
+	{
+		while(global_g == 0)
+		{
+			TimerSet(700);
+			TimerOn();
+			TickFct_State_machine_1();
+			while(!TimerFlag);
+			TimerFlag = 0;	
+		}
+		while(global_g == 1)
+		{
+			TimerSet(200);
+			TimerOn();
+			ButtonPress();
+			while(!TimerFlag);
+			TimerFlag = 0;
+		}
+		while(global_g == 2)
+		{
+			TimerSet(1000);
+			TimerOn();
+			TickFct_Machine2();
+			while(!TimerFlag);
+			TimerFlag = 0;
+		}
+		while(global_g == 3)
+		{
+			TimerSet(400);
+			TimerOn();
+			ButtonPress2();
+			while(!TimerFlag);
+			TimerFlag = 0;
+		}
+		while(global_g == 4)
+		{
+			TimerSet(1000);
+			TimerOn();
+			TickFct_Machine3();
+			while(!TimerFlag);
+			TimerFlag = 0;
+		}
+		while(global_g == 5)
+		{
+			TimerSet(400);
+			TimerOn();
+			ButtonPress3();
+			while(!TimerFlag);
+			TimerFlag = 0;
+		}
+	}
+	return 0;
+		
+}
